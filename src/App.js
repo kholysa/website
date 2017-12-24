a50a01280b322fa743ae0b316c9c96b1cb546886
Saif Elkholy <saif@districtm.ca>
2017-12-24 01:46:11 -0500

    Added full screen spotlight thingy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './style/App.css';
 import Header from './Header';
+import TheatreImage from './TheatreImage';
 import Body from "./Body";
 import Footer from "./Footer";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <TheatreImage/>
           <Header/>
           <Body/>
           <Footer/>
