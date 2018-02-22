b0da6d49d2811cae351f17e5534cf7a2596b8c2c
Saif Elkholy <saif@districtm.ca>
2018-02-21 21:24:17 -0500

    Added particles.js

--- src/TheatreImage/index.js
+++ src/TheatreImage/index.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import '../Style/TheatreImage.css'
+import Particles from 'react-particles-js';
+import '../Style/TheatreImage.css';
 import Menus from './Menus'
 
 class TheatreImage extends Component {
     render() {
         return (
-    <div>
+    <div className="Theatre-image">
         <div className="Menu-wrapper">
             <Menus/>
         </div>
-            <div className="Theatre-image">
-                <div className="Intro-text">
-                    <h4><b>Hey there!</b></h4>
-                    Scroll down to learn a bit more about me, Saif
-                </div>
-            </div>
+        <Particles
+            params={{
+                particles: {
+                    line_linked: {
+                        shadow: {
+                            enable: true,
+                            color: "#000",
+                            blur: 1
+                        }
+                    },
+                    number: {
+                        value: 75
+                    },
+
+                }
+            }}
+            className="Particles-overlay"
+        >
+
+
+        </Particles>
+        <div className="Intro-text">
+            <h4><b>Hey there!</b></h4>
+            Scroll down to learn a bit more about me, Saif
         </div>
+    </div>
         );
     }
 }
