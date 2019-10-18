825caa74f26a23fbdbf87b63998522262db019fe
kholy <kholysa@gmail.com>
2019-10-17 23:19:02 -0400

    Revert "Added particles.js"
    This reverts commit b0da6d49

--- src/TheatreImage/index.js
+++ src/TheatreImage/index.js
@@ -1,41 +1,21 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
-import '../Style/TheatreImage.css';
+import '../Style/TheatreImage.css'
 import Menus from './Menus'
 
 class TheatreImage extends Component {
     render() {
         return (
-    <div className="Theatre-image">
+    <div>
         <div className="Menu-wrapper">
             <Menus/>
         </div>
-        <Particles
-            params={{
-                particles: {
-                    line_linked: {
-                        shadow: {
-                            enable: true,
-                            color: "#000",
-                            blur: 1
-                        }
-                    },
-                    number: {
-                        value: 75
-                    },
-
-                }
-            }}
-            className="Particles-overlay"
-        >
-
-
-        </Particles>
-        <div className="Intro-text">
-            <h4><b>Hey there!</b></h4>
-            Scroll down to learn a bit more about me, Saif
+            <div className="Theatre-image">
+                <div className="Intro-text">
+                    <h4><b>Hey there!</b></h4>
+                    Scroll down to learn a bit more about me, Saif
+                </div>
+            </div>
         </div>
-    </div>
         );
     }
 }
