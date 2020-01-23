68ff6e85cb0fa17393bbe9022a1a2fba90491651
saif <kholysa@gmail.com>
2020-01-23 15:47:53 +0100

    Uploaded build folder because my silly canadian green energy server doesnt like me being root (and installing npm)

---
+++ build/precache-manifest.f21a8f2562808dd4f25e33ff509ca493.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e52988e34628a0c76eaf06404d6cbef",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "725034dfd9a879e06b52",
+    "url": "/website/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "3eae8b5eed29e1cf298e",
+    "url": "/website/static/css/main.5137a758.chunk.css"
+  },
+  {
+    "revision": "725034dfd9a879e06b52",
+    "url": "/website/static/js/2.fb3b605e.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/website/static/js/2.fb3b605e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3eae8b5eed29e1cf298e",
+    "url": "/website/static/js/main.932257bb.chunk.js"
+  },
+  {
+    "revision": "2662ebf5b737d07e72a8",
+    "url": "/website/static/js/runtime-main.763deec5.js"
+  },
+  {
+    "revision": "b2bcb4cad08eaa17a5c4a314969a12c0",
+    "url": "/website/static/media/Elkholy_Saif_CV_Fall_2019.b2bcb4ca.pdf"
+  },
+  {
+    "revision": "d0593e51010baffb1dde27e0f7edf258",
+    "url": "/website/static/media/codeJam.d0593e51.png"
+  },
+  {
+    "revision": "f678e26725c4e5170c7516f26b1b7d74",
+    "url": "/website/static/media/dpmmain.f678e267.jpg"
+  },
+  {
+    "revision": "b0d96e3f0bae9292c3ddaed4e57e86bf",
+    "url": "/website/static/media/mchacks.b0d96e3f.png"
+  },
+  {
+    "revision": "fdd898a60bc368caa89fda0e6a6e424c",
+    "url": "/website/static/media/mmirrormain.fdd898a6.jpg"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/website/static/media/react-hexagon.04557c1b.png"
+  },
+  {
+    "revision": "2dd43e7cdc9d00a88ae462a33c60435b",
+    "url": "/website/static/media/spotlight.2dd43e7c.jpg"
+  }
+]);
